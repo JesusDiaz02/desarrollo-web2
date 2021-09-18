ea8644afc29a7554895695b079f03b7b0cfb1be5
Jesus Diaz <jesusdiazg9@gmail.com>
2021-09-18 08:10:47 -0500

    commit tres

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { UserTypesModule } from './user-types/user-types.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 
 @Module({
-  imports: [UsersModule, UserTypesModule],
+  imports: [UsersModule, UserTypesModule, 
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '127.0.0.1',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'apicuc',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
